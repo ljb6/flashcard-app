8c86197846f11924a49ce2241e63d82f18c6d31d
Lucca <luccajbecker@gmail.com>
2025-05-20 20:02:20 -0300

    feat: add basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,12 @@
 <script>
-    import Flashcard from "../components/Flashcard.svelte";
+
 </script>
 
-<Flashcard front="Qual é a capital do acre?" back="Rio Branco"/>
+<div class="flex flex-col items-center justify-center min-h-screen bg-blue-50">
+    <h1 class="text-5xl font-bold text-gray-800 mb-8">FlashApp</h1>
+    
+    <div class="flex justify-center gap-8">
+        <a href="/" class="text-blue-600 hover:text-blue-800 font-medium px-6 py-3 rounded-lg hover:bg-blue-100 transition-colors shadow-md">Praticar</a>
+        <a href="/" class="text-blue-600 hover:text-blue-800 font-medium px-6 py-3 rounded-lg hover:bg-blue-100 transition-colors shadow-md">Gerenciar Flashcards</a>
+    </div>
+</div>
