c49f22cd226bc5b44043f9de800f4994a125e487
Lucca <luccajbecker@gmail.com>
2025-05-20 19:54:59 -0300

    feat: add basic flashcard component

--- src/components/Flashcard.svelte
+++ src/components/Flashcard.svelte
@@ -1,1 +1,56 @@
+<script>
+    let { front, back } = $props();
+    let showAnswer = $state();
+    function handleFlashcard() {
+        showAnswer = !showAnswer;
+    }
+</script>
 
+<div class="flex items-center justify-center min-h-screen bg-gray-100">
+    <!-- svelte-ignore a11y_click_events_have_key_events -->
+    <div class="perspective">
+        <!-- svelte-ignore a11y_no_static_element_interactions -->
+        <div
+            class="flashcard w-90 h-90 rounded-xl shadow-lg cursor-pointer transition-transform duration-500"
+            class:flipped={showAnswer}
+            onclick={handleFlashcard}
+        >
+            <div
+                class="front absolute w-full h-full flex items-center justify-center bg-white rounded-xl border-2 border-gray-200 backface-hidden"
+            >
+                <p class="text-2xl font-semibold text-gray-800">
+                    {front}
+                </p>
+            </div>
+            <div
+                class="back absolute w-full h-full flex items-center justify-center bg-blue-50 rounded-xl border-2 border-blue-400 backface-hidden rotate-y-180"
+            >
+                <p class="text-2xl font-semibold text-blue-600">{back}</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<style>
+    .perspective {
+        perspective: 1000px;
+    }
+    .flashcard {
+        position: relative;
+        transform-style: preserve-3d;
+        transition: transform 0.5s;
+    }
+    .flashcard .front,
+    .flashcard .back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+    }
+    .flashcard .back {
+        transform: rotateY(180deg);
+    }
+    .flipped {
+        transform: rotateY(180deg);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import Flashcard from "../components/Flashcard.svelte";
+</script>
+
+<Flashcard front="Qual é a capital do acre?" back="Rio Branco"/>
