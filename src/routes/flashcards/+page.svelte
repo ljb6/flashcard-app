9611b35d181ae77b761afe7bcca89192bae32be5
Lucca <luccajbecker@gmail.com>
2025-05-21 10:15:43 -0300

    style: add cursor-pointer

--- src/routes/flashcards/+page.svelte
+++ src/routes/flashcards/+page.svelte
@@ -7,17 +7,17 @@
     <div class="flex justify-center gap-4">
         <a
             href="/"
-            class="bg-blue-100 text-blue-700 hover:bg-blue-200 hover:text-blue-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+            class="cursor-pointer bg-blue-100 text-blue-700 hover:bg-blue-200 hover:text-blue-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
             >Praticar</a
         >
         <button
             onclick={() => (showCreateFlashcardPopup = true)}
-            class="bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+            class="cursor-pointer bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
             >Criar</button
         >
         <a
             href="/"
-            class="bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+            class="cursor-pointer bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
             >Deletar todos</a
         >
     </div>
