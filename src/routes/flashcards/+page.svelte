6f85fc11299f22902b0f4c5ee8f718c37c3bf2bf
Lucca <luccajbecker@gmail.com>
2025-05-21 11:02:29 -0300

    style: flashcards page style

--- src/routes/flashcards/+page.svelte
+++ src/routes/flashcards/+page.svelte
@@ -23,22 +23,25 @@
 </script>
 
 <div class="flex flex-col items-start ml-2 mt-2 bg-white p-2">
-    <div class="flex justify-center gap-4">
-        <a
-            href="/"
-            class="cursor-pointer bg-blue-100 text-blue-700 hover:bg-blue-200 hover:text-blue-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
-            >Praticar</a
-        >
-        <button
-            onclick={() => (showCreateFlashcardPopup = true)}
-            class="cursor-pointer bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
-            >Criar</button
-        >
-        <a
-            href="/"
-            class="cursor-pointer bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
-            >Deletar todos</a
-        >
+    <div class="flex justify-between items-center gap-4 w-full">
+        <div class="flex gap-4">
+            <a
+                href="/"
+                class="cursor-pointer bg-blue-100 text-blue-700 hover:bg-blue-200 hover:text-blue-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+                >Praticar</a
+            >
+            <button
+                onclick={() => (showCreateFlashcardPopup = true)}
+                class="cursor-pointer bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+                >Criar</button
+            >
+            <a
+                href="/"
+                class="cursor-pointer bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+                >Deletar todos</a
+            >
+        </div>
+        <h2 class="mr-5 text-xl">Flashcards: {flashcards.length}</h2>
     </div>
 </div>
 <hr class="mt-2 border-blue-100" />
@@ -48,17 +51,23 @@
     onClose={() => (showCreateFlashcardPopup = false)}
 />
 
-<div class="flashcard-grid justify-center mt-4 ml-4 mr-4">
-    {#each flashcards as flashcard}
-        <Flashcard front={flashcard.Front} back={flashcard.Back} />
-    {/each}
-</div>
-
-<style>
-    .flashcard-grid {
-        display: grid;
-        grid-template-columns: repeat(5, 1fr);
-        gap: 0.5rem;
-        justify-items: center;
-    }
-</style>
+<section class="bg-white p-6 rounded-2xl">
+    <table class="w-full text-left">
+        <thead>
+            <tr class="border-b border-[#E0E0E0]">
+                <th class="pb-4">ID</th>
+                <th class="pb-4">Frente</th>
+                <th class="pb-4">Verso</th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each flashcards as flashcard}
+                <tr class="border-b border-[#F0F0F0]">
+                    <td class="py-3">{flashcard.ID}</td>
+                    <td class="py-3">{flashcard.Front}</td>
+                    <td class="py-3">{flashcard.Back}</td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
+</section>
