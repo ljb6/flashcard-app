5b1e29940b96f656df01958e7e22ddfaf1e9a923
Lucca <luccajbecker@gmail.com>
2025-05-21 09:37:58 -0300

    feat: add buttons to manage flashcards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
-<script>
-
-</script>
-
 <div class="flex flex-col items-center justify-center min-h-screen bg-blue-50">
     <h1 class="text-5xl font-bold text-gray-800 mb-8">FlashApp</h1>
-    
+
     <div class="flex justify-center gap-8">
-        <a href="/" class="text-blue-600 hover:text-blue-800 font-medium px-6 py-3 rounded-lg hover:bg-blue-100 transition-colors shadow-md">Praticar</a>
-        <a href="/" class="text-blue-600 hover:text-blue-800 font-medium px-6 py-3 rounded-lg hover:bg-blue-100 transition-colors shadow-md">Gerenciar Flashcards</a>
+        <a
+            href="/"
+            class="text-blue-600 hover:text-blue-800 font-medium px-6 py-3 rounded-lg hover:bg-blue-100 transition-colors shadow-md"
+            >Praticar</a
+        >
+        <a
+            href="/flashcards"
+            class="text-blue-600 hover:text-blue-800 font-medium px-6 py-3 rounded-lg hover:bg-blue-100 transition-colors shadow-md"
+            >Gerenciar Flashcards</a
+        >
     </div>
 </div>
---
+++ src/routes/flashcards/+page.svelte
@@ -0,0 +1,17 @@
+<div class="flex flex-col items-start ml-2 mt-2 bg-white p-2">
+    <div class="flex justify-center gap-4">
+        <a
+            href="/"
+            class="bg-blue-100 text-blue-700 hover:bg-blue-200 hover:text-blue-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+        >Praticar</a>
+        <a
+            href="/"
+            class="bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+        >Criar</a>
+        <a
+            href="/"
+            class="bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-900 font-medium px-6 py-3 rounded-lg transition-colors shadow-md"
+        >Deletar todos</a>
+    </div>
+</div>
+<hr class="mt-2 border-blue-100">
