e5e0683339027018707ea52061c027d0cf5c6975
Lucca <luccajbecker@gmail.com>
2025-05-21 09:56:04 -0300

    feat: add CreateFlashcard popup

---
+++ src/components/CreateFlashcard.svelte
@@ -0,0 +1,35 @@
+<script>
+    let { open, onClose } = $props();
+</script>
+
+{#if open}
+    <div
+        class="fixed inset-0 bg-opacity-40 flex items-center justify-center z-50"
+    >
+        <div class="bg-blue-50 p-8 rounded-lg shadow-lg min-w-[320px]">
+            <h2 class="text-lg font-bold mb-4 text-blue-700">
+                Criar Flashcard
+            </h2>
+            <form>
+                <input
+                    class="border border-blue-200 rounded-lg p-2 w-full mb-3 focus:outline-none focus:ring-2 focus:ring-blue-200"
+                    placeholder="Frente"
+                />
+                <input
+                    class="border border-blue-200 rounded-lg p-2 w-full mb-3 focus:outline-none focus:ring-2 focus:ring-blue-200"
+                    placeholder="Verso"
+                />
+                <button
+                    type="submit"
+                    class="cursor bg-green-100 text-green-700 hover:bg-green-200 hover:text-green-900 font-medium px-6 py-2 rounded-lg transition-colors shadow-md mr-2"
+                    >Salvar</button
+                >
+                <button
+                    type="button"
+                    class="cursor bg-red-100 text-red-700 hover:bg-red-200 hover:text-red-900 font-medium px-6 py-2 rounded-lg transition-colors shadow-md"
+                    onclick={onClose}>Fechar</button
+                >
+            </form>
+        </div>
+    </div>
+{/if}
